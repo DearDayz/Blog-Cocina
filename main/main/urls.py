8321f6244f2fcbb5e1122ebcc66b660f72cf91df
Alvarezzzzz <alejandrojalvarezg.2005@gmail.com>
2024-09-28 07:51:23 -0400

    se crean los modulos en el back

--- main/main/urls.py
+++ main/main/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ejemplo project.
+URL configuration for main project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
