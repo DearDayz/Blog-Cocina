11cde320114c0c310113ea2704a1276dc0450cad
alemt19 <alfredomt1906@gmail.com>
2024-10-05 16:36:27 -0400

    migrations

--- backend/main/login3/urls.py
+++ backend/main/login3/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("login/", views.login_user, name="login"),
     path("register/", views.RegisterUser.as_view(), name="register"),
     path("logout/", views.logout_user, name="logout"),
-    path("permissions/", views.user_permissions, name="permissions"),
+    #path("permissions/", views.user_permissions, name="permissions"),
     path("userdata/<str:username>/", views.UserDataView, name="userdata"), # Recibe peticiones POST y PUT
 ]
