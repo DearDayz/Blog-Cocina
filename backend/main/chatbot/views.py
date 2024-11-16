ef10fd76717be875f1db0182ca1ebd3446ac6f93
alemt19 <alfredomt1906@gmail.com>
2024-11-16 12:06:45 -0400

    cambios viejos pendientes de pushear y creación de nueva rama

--- backend/main/chatbot/views.py
+++ backend/main/chatbot/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from .context import cargar_contexto_json_cliente, contexto_json_a_string
+import requests
 
 # Create your views here.
 
 def chatbot(request):
-    # response = requests.get("http://127.0.0.1:8000/blog/recetas/")
-    # tabla = {"recetas": response.json()}
-    # cargar_contexto_json_cliente(tabla)
+    response = requests.get("http://127.0.0.1:8000/blog-api/recetas/")
+    print(response.json())
+    tabla = {"recetas": response.json()}
+    cargar_contexto_json_cliente(tabla)
+    contexto_json_a_string()
     return render(request, "chat/chatbot.html")
