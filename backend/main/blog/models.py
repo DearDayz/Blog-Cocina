9539d52e44a1fd9eb2c91338a379415e761bc630
Alvarezzzzz <alejandrojalvarezg.2005@gmail.com>
2024-11-18 21:36:27 -0400

    Modelo de valoreciones agregado

--- backend/main/blog/models.py
+++ backend/main/blog/models.py
@@ -4,6 +4,7 @@ from ecommerce.models import Producto
 from .validadores.validador_receta import validar_solo_letras_con_espacio, validate_no_html, validate_between_zero_and_five,validate_cantidades
 from django.core.validators import MaxLengthValidator
 from login3.models import MyUser
+from django.db.models.signals import post_save
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -19,7 +20,7 @@ class Receta(models.Model):
     descripcion = models.TextField(verbose_name="descripcion", validators=[MaxLengthValidator(2000), validate_no_html])
     preparacion = models.TextField(verbose_name="preparacion", validators=[MaxLengthValidator(5000), validate_no_html])
     imagen = models.ImageField(upload_to='recetas/', blank=True, null=True, verbose_name="Imagen")
-    puntuacion = models.FloatField(verbose_name="puntuacion", validators=[validate_between_zero_and_five])
+    puntuacion = models.FloatField(verbose_name="puntuacion", validators=[validate_between_zero_and_five], blank=True)
     category = models.ManyToManyField(Category, related_name='recetas')
     date_modified = models.DateTimeField(auto_now=True)
     
@@ -30,6 +31,17 @@ class Receta(models.Model):
         if self.imagen and self.imagen.name:
             self.imagen.storage.delete(self.imagen.name)  # Solo eliminar la imagen si existe
         super().delete(using=using, keep_parents=keep_parents)  # Siempre llama al método de eliminación del padre
+
+    
+    def get_puntuacion(self):
+        valoraciones = Valoracion.objects.filter(receta=self)
+        if valoraciones.exists():
+            self.puntuacion = round(valoraciones.aggregate(models.Avg('puntuacion'))['puntuacion__avg'], 1)
+            self.save()
+
+        else:
+            self.puntuacion = 0
+            self.save()
 
 class Ingrediente(models.Model):
     id = models.AutoField(primary_key=True)
@@ -48,5 +60,17 @@ class Ingrediente(models.Model):
 
 
 
-class Valoracion (models.Model):
+class Valoracion(models.Model):
     user = models.ForeignKey(MyUser, on_delete=models.CASCADE,  verbose_name="usuario", to_field="cedula")
+    receta = models.ForeignKey(Receta, on_delete=models.CASCADE, verbose_name="receta")
+    puntuacion = models.PositiveIntegerField(verbose_name="puntuacion")
+
+    class Meta:
+        verbose_name_plural = "valoraciones"
+
+def create_puntuacion(sender, instance, created, **kwargs):
+    val = instance
+    receta = val.receta
+    receta.get_puntuacion()
+
+post_save.connect(create_puntuacion, sender=Valoracion)
